6fe0cae0f68a7c9839ff1bc87a7ff1377807ff9c
keerthana360 <72611653+keerthana360@users.noreply.github.com>
2020-10-18 22:23:26 +0530

    Add files via upload

---
+++ pharmabot.py
@@ -0,0 +1,46 @@
+import random
+import webbrowser
+from googlesearch import search
+def greet():
+    greetings = ["Hi buddy! I am pharmabot.I can help you in knowing details about any medicine.May I know your name?","Wonderful.It is so nice to be in touch with you.I am a pharmabot.May I know your name?"]
+    print(random.choice(greetings))
+def welcome(name):
+    welcome_message = ["Nice to meet you","Lets have some good time together"]
+    print()
+    print("{0} {1}".format(random.choice(welcome_message),name))
+def menu():
+    print()
+    print("1.Get info about medicine")
+    print("2.End this chat.")
+    print("-------------------------")
+    print()
+    choice=int(input("Enter your choice: "))
+    if 1<=choice<=2:
+        return choice
+    else:
+        return 0
+def details_medicine():
+    print()
+    query=input("Enter name of the medicine: ")
+    query=query+ "medicine"
+    for j in search(query,tld="com",num=1,stop=1,pause=0):
+        webbrowser.open(j)
+def pharmabot():
+    greet()
+    name=input("Your name: ")
+    welcome(name)
+    option = menu()
+    while option != 2:
+        if option == 1:
+            details_medicine()
+        else:
+            print()
+            print("Enter choice between 1 and 2.")
+            print()
+        option = menu()
+    if option==2:
+        print()
+        print("Thanks for interacting with me,bye...")
+pharmabot()
+
+
